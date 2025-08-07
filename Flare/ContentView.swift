06b1bd399f1ab46f37b6e8e71f2d5dd66209ef29
Ritesh <riteshkhore@gmail.com>
2025-08-07 17:40:31 +0530

    Initial Commit

---
+++ Flare/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Flare
+//
+//  Created by Ritesh Khore on 07/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Flare/FlareApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlareApp.swift
+//  Flare
+//
+//  Created by Ritesh Khore on 07/08/25.
+//
+
+import SwiftUI
+
+@main
+struct FlareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
